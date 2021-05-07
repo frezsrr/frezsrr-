86a3b64d6d73268057d53f6947d06380201e3c50
frezsrr <82252666+frezsrr@users.noreply.github.com>
2021-05-07 18:52:39 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,30 @@
+const menuadmin = (prefix, pushname) => {
+ return `Sai fora membro comum, esses comandos
+não funcionam Contigo.
+Atenção!!! Para esses comandos funcionar o bot deve ser adm
+
+ 
+ ◪ *COMANDO DOS ADMINS*
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote
+  ├─ ❏ ${prefix}demote
+  ├─ ❏ ${prefix}tagall
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}listadmins
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+  └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/ps3.js
@@ -0,0 +1,38 @@
+const ps3 = (prefix, sender) => {
+	return `Menu do PS3
+
+╭──────────────╮
+ *𝕄𝔼ℕ𝕌 𝔻𝕆 ℙ𝕊3*
+╰──────────────╯
+Link de tudo que você precisara para o PS3 Desbloqueado
+
+Como Instalar o Hen:
+https://youtu.be/r4D6Hx-jsX8
+
+Como Desbloquear o Ps3 em Cfw: https://youtu.be/aHt7qU9n_Ns
+
+Hfw 4.87: https://mega.nz/file/fAJFwSqL#PeQfhVjbGAi69S4Jr369Tly4trG4Y9ALqdlUIBzW8qk
+
+Cfw Rebug Rex 4.84: https://playstationscenefiles.com/rebug/REBUG_4.84.2_REX_0835d81e3c581f3bdfdfbe86fca5e192_PS3UPDAT.PUP
+
+Cfw Ferrox 4.87: https://mega.nz/file/qtlVEAJa#lCzHzgzU5I1m21UcscAkeB5Mujl-dS10lxSCQ8ekh9M
+
+Multiman 04.85.01: https://store.brewology.com/get/homebrew.php?id=24&fid=2200
+
+Como Converter o GTA Midia Digital para Bles: https://youtu.be/GsYJy2J6SCk
+
+Webman 1.47.34: https://store.brewology.com/get/homebrew.php?id=310&fid=2227
+(Obs: Instalem sempre a versão lite, a Full é muito pesada e pode trazer aquecimento e tela preta em alguns jogos do ps3)
+
+Homebrews: https://store.brewology.com/homebrew.php?lang=
+
+Loja Pkgi (by: bucanero): https://github.com/bucanero/pkgi-ps3/releases/download/v1.1.8/pkgi-ps3.pkg
+
+Lista de Jogos PKGI + Raps: http://www.mediafire.com/file/yitowrjb63p1vv2/Lista+jogos+pkgi+atualizada.pkg/file
+
+14mil licenças pkg: https://www.mediafire.com/file/1a4e90i0jn0u1hs/14k_@DIOGOPLATER.rar/file
+
+Sejam Felizes Galera, e se cuidem em cuidado com a covid ksks`
+}
+
+exports.ps3 = ps3
